576b990e94775d0129b9f47070bbc40d9c7eb4a2
nushee <thomas.latour@hetic.net>
2022-03-11 10:43:49 +0100

    css

--- app/wp-content/themes/ProjetBnB/footer.php
+++ app/wp-content/themes/ProjetBnB/footer.php
@@ -1,4 +1,23 @@
 <?php
 ?>
+<footer>
+    <div id = "footer">
+        <section id = "logo-social">
+            <img src="" alt="photo du Logo" id = "logo">
+            <p id = "siege">Siège social : 16 avenue du Général de Gaulle </br>
+                75011 Paris</p>
+        </section>
+        <section id = "separation"></section>
+        <section id = "services">
+            <p id = "service">Nos services</p>
+            <p id = "recherche">Recherche par villes </br>
+            Nos différentes offres</p>
+            <p id = "poster">Poster une offre</br>
+            Favoris</p>
+        </section>
+        <p id = "droit">© 2022 Nom du site. Tous droits réservés.</br>
+        Confidentialité - Conditions générales - Mentions Légales - Contact - A propos</p>
+    </div>
+</footer>
 </body>
 </html>
--- app/wp-content/themes/ProjetBnB/functions.php
+++ app/wp-content/themes/ProjetBnB/functions.php
@@ -1,3 +1,10 @@
 <?php
 add_theme_support('title-tag');
+
+add_action('wp_enqueue_scripts','footer');
+
+function footer ()
+{
+    wp_enqueue_style('maFeuilleCSSFooter',get_stylesheet_directory_uri()."footer.css");
+}
 ?>
